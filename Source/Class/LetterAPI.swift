540a765236e47ede756f3c2c7d024937a7ad0f7c
Vasily Ulianov <vasily@me.com>
2018-04-04 11:41:09 +0300

    XCode update

--- Source/Class/LetterAPI.swift
+++ Source/Class/LetterAPI.swift
@@ -10,6 +10,10 @@ import Foundation
 
 public class LetterAPI {
 	
+	/// Make request to Letter API.
+	///
+	/// - Parameters:
+	///   - request: `LetterAPIRequest`
 	public static func make<RequestModel: LetterAPIRequest>(request: RequestModel, completion: @escaping (Result<RequestModel.AnswerModel>) -> Void) {
 		DispatchQueue.global().async {
 			let operation = LetterAPIOperation(request: request)
