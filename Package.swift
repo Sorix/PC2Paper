517068fddc97e917053f68283889e5901741f450
Vasily Ulianov <vasily@me.com>
2018-04-22 15:35:09 +0300

    Add Swift Package

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:4.0
+
+import PackageDescription
+
+let package = Package(
+	name: "PC2Paper",
+	products: [
+		.library(name: "PC2Paper", targets: ["PC2Paper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/drmohundro/SWXMLHash.git", .upToNextMajor(from: "4.6.0"))
+		],
+	targets: [
+		.target(name: "PC2Paper", dependencies: ["SWXMLHash"], path: "Source")
+	]
+)
+
